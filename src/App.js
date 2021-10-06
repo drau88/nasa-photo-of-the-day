d1c3e353ff4faf7ee598b57ff7a837132d64ece0
Daniel Rau <drau88@cox.net>
2021-10-06 13:47:35 -0700

    Succeeded in causing API Data and a main display component to render.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import MainDisplay from './maindisplay';
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then(response => {
-        console.log(response);
-    }).catch(err => {
-        console.error(err);
-    });
+
 
 function App() {
+
+const [apiData, setApiData] = useState({});
+
+useEffect(() => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(response => {
+      console.log(response);
+      setApiData(response.data);
+  }).catch(err => {
+      console.error(err);
+  });
+}, [])
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-        <MainDisplay></MainDisplay>
       </p>
+      <MainDisplay apiData={apiData}></MainDisplay>
     </div>
   );
 }
--- src/maindisplay.js
+++ src/maindisplay.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+
+
 const MainDisplay = (props) => {
 
+    const { apiData } = props;
+    console.log(props);
     return (
         <div className='displayContainer'>
-            asdf
+            <img src={apiData.url} alt='Amazing space pic'></img>
+            <h2>Credit goes to: {apiData.copyright} </h2>
+            <h2>What am I looking at?</h2>
+            <p>{apiData.explanation}</p>
         </div>
 
     )
